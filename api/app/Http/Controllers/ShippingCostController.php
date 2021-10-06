f0b15cb3207c585c1b883b84c2013e1c14e48a12
Santiago Rodriguez <santiagojrodriguez@gmail.com>
2021-10-06 16:01:45 -0300

    .
    .

---
+++ api/app/Http/Controllers/ShippingCostController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ShippingCost;
+use Illuminate\Http\Request;
+
+class ShippingCostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    public function calculateCost()
+    {
+        $costs = ShippingCost::all();
+        return response()->json($costs);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ShippingCost  $shippingCost
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ShippingCost $shippingCost)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ShippingCost  $shippingCost
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ShippingCost $shippingCost)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ShippingCost  $shippingCost
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ShippingCost $shippingCost)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ShippingCost  $shippingCost
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ShippingCost $shippingCost)
+    {
+        //
+    }
+}
