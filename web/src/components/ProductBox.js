05eb04460cbdb25416e5749f7600b162b500b8eb
varelajm33 <99084846+varelajm33@users.noreply.github.com>
2022-04-22 18:36:12 +0300

    ajustes carrito 22.04.22

--- web/src/components/ProductBox.js
+++ web/src/components/ProductBox.js
@@ -20,9 +20,9 @@ const ProductBox = (props) => {
       props.product.weight
     );
     setAddedMessage(true);
-    setTimeout(() => {
-      setAddedMessage(false);
-    }, 1000);
+    //setTimeout(() => {
+      //setAddedMessage(false);
+    //}, 1000);
   };
 
   const handleBuyItem = () => {
@@ -65,37 +65,53 @@ const ProductBox = (props) => {
           
         </div>
 
-        <div className="stats_box text-center">
+       
+
+
+<div className="row">
+<div className="col-md-6 columnaizq"> 
+
+ <div className="stats_box text-center">
           <span className="stats">
             {props.product.stat_players} | {props.product.stat_age} |{" "}{props.product.stat_time}
           </span>
         </div>
 
-        <div>
-          <p className="short_description">
-            {props.product.short_description}
-          </p>
-        </div>
 
-        <div className="price_row">
+<p className="short_description">
+            {props.product.short_description}
+          </p> </div>
+<div className="col-md-6 columnader"><div className="price_row">
           <div className="product_price">$ {props.product.price}</div>
-        </div>
-        
-        <div className="price_row">
+        </div> 
+ <div className="price_row">
        {/*   <button className="btn btn-danger btn-buy" id={"btn-buy-"+props.product.id} onClick={handleBuyItem}>
               <span className="buy_label">Comprar ahora</span>
             </button>
             &nbsp;*/}
-
-
-
-
-            <button className="btn btn-danger btn-buy" id={"btn-buy-"+props.product.id} onClick={handleAddItem}>
-              <span className="buy_label">Agregar al <span className="icocart"> <HiShoppingCart /></span></span>
+  <button className="btn btn-danger btn-buy" id={"btn-buy-"+props.product.id} onClick={handleAddItem}>
+              <span className="buy_label">agregar al <span className="icocart"> <HiShoppingCart /></span></span>
             </button>
         </div>
+</div>
+</div>
 
-        <div className="added_row">{addedMessage && <span>¡Agregado!</span> }</div>
+
+
+
+
+      
+
+        
+        
+       
+
+
+
+
+          
+
+        <div className="added_row">{addedMessage && <span><p className="agregado"> ¡Agregado al carrito!</p></span> }</div>
 
       
 
