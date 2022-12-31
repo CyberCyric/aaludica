232de228f99dcf13d84692e645045d0ebfea3264
Cyber Cyric <santiagojrodriguez@gmail.com>
2022-12-31 10:50:20 -0300

    Boton de link de catalogo en Header

--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -1,34 +1,34 @@
-import React,{ useContext } from "react";
-import { Navbar, Container, Nav, NavDropdown, Button  } from "react-bootstrap";
-import { HiShoppingCart, HiMail } from "react-icons/hi";
+import React, { useContext } from "react";
+import { Navbar, Container, Nav, NavDropdown, Button } from "react-bootstrap";
+import { HiShoppingCart, HiMail, HiColorSwatch } from "react-icons/hi";
 import { CartContext } from "../contexts/CartContext";
-import {
-    Link
-  } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    let cart = useContext(CartContext);
-    return (
-        <header> 
-            <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
-              <Container>
-                <Navbar.Brand>
-                  <Link to="/">
-                    <img
-                      src="/images/aa-logo-navbar.png"
-                      className="img-responsive navbar-logo"
-                      data-original-title=""
-                      alt="logo"
-                    />
-                    <div className="tagline-txt"><div className="logo-txt">AA Lúdica </div> 
-                    <div className="tagline"> Juegos de Mesa</div></div>
-                  </Link>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                {
-                  <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                    {/*
+  let cart = useContext(CartContext);
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+        <Container>
+          <Navbar.Brand>
+            <Link to="/">
+              <img
+                src="/images/aa-logo-navbar.png"
+                className="img-responsive navbar-logo"
+                data-original-title=""
+                alt="logo"
+              />
+              <div className="tagline-txt">
+                <div className="logo-txt">AA Lúdica </div>
+                <div className="tagline"> Juegos de Mesa</div>
+              </div>
+            </Link>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          {
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                {/*
                       <NavDropdown
                         title="Sobre nosotros"
                         id="basic-nav-dropdown2"
@@ -59,27 +59,35 @@ const Header = () => {
                         </NavDropdown.Item>
                       </NavDropdown>
                     */}
-                      <Nav.Link href="/catalog">Catálogo</Nav.Link>
-                    </Nav>
-                    <Nav>
-                      <Button variant="secondary" className="navbar_button">
-                        <Nav.Link href="/cart">
-                          <HiShoppingCart /> 
-                          {cart.items.length>0 && <span className="badge bg-danger">{cart.items.length}</span> }
-                        </Nav.Link>
-                      </Button>
-                      <Button variant="secondary" className="navbar_button">
-                        <Nav.Link href="/contact">
-                          <HiMail /> 
-                        </Nav.Link>
-                      </Button>
-                    </Nav>
-                  </Navbar.Collapse>
-                }
-              </Container>
-            </Navbar>
-          </header>
-    );
+              </Nav>
+              <Nav>
+                <Button variant="secondary" className="navbar_button">
+                  <Nav.Link href="/catalog">
+                    <HiColorSwatch />
+                  </Nav.Link>
+                </Button>
+                <Button variant="secondary" className="navbar_button">
+                  <Nav.Link href="/cart">
+                    <HiShoppingCart />
+                    {cart.items.length > 0 && (
+                      <span className="badge bg-danger">
+                        {cart.items.length}
+                      </span>
+                    )}
+                  </Nav.Link>
+                </Button>
+                <Button variant="secondary" className="navbar_button">
+                  <Nav.Link href="/contact">
+                    <HiMail />
+                  </Nav.Link>
+                </Button>
+              </Nav>
+            </Navbar.Collapse>
+          }
+        </Container>
+      </Navbar>
+    </header>
+  );
 };
 
 export default Header;
