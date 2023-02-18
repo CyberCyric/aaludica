ff91067fbd58f7ea3e21a9038aeea25033e5664a
Cyber Cyric <santiagojrodriguez@gmail.com>
2023-02-18 12:55:10 -0300

    subname en Producto

--- web/src/components/ProductBox.js
+++ web/src/components/ProductBox.js
@@ -35,70 +35,78 @@ const ProductBox = (props) => {
       props.product.weight
     );
     history.push("/cart");
-  }
+  };
 
   return (
     <div className="col-xl-4 col-md-6 mb-5">
       <div className="template_card d-flex flex-column">
         {props.product.is_new === "Y" && (
-          <div className="label new">
-            Recién llegado
-          </div>
+          <div className="label new">Recién llegado</div>
         )}
         {props.product.is_last_units === "Y" && (
-          <div className="label stockout">
-            Quedan poquitos...
-          </div>
+          <div className="label stockout">Quedan poquitos...</div>
         )}
         {props.product.is_combo === "Y" && (
-          <div className="label combo">
-            Combo
-          </div>
+          <div className="label combo">Combo</div>
         )}
         <div className="product_box_image">
           <img src={`/images/products/${props.product.main_photo}`} />
         </div>
 
-
         <div className="titleproducts" align="center">
           {props.product.name}
-
         </div>
 
-
-
+        <div className="subtitleproducts" align="center">
+          {props.product.subname}
+        </div>        
 
         <div className="row">
           <div className="col-md-6 columnaizq">
-
             <div className="stats_box text-center">
               <span className="stats">
-                {props.product.stat_players} | {props.product.stat_age} |{" "}{props.product.stat_time}
+                {props.product.stat_players} | {props.product.stat_age} |{" "}
+                {props.product.stat_time}
               </span>
             </div>
-
-
             <p className="short_description">
               {props.product.short_description}
-            </p> </div>
-          <div className="col-md-6 columnader"><div className="price_row">
-            <div className="product_price">$ {props.product.price}</div>
+            </p>{" "}
           </div>
+          <div className="col-md-6 columnader">
+            <div className="price_row">
+              <div className="product_price">$ {props.product.price}</div>
+            </div>
             <div className="price_row">
               {/*   <button className="btn btn-danger btn-buy" id={"btn-buy-"+props.product.id} onClick={handleBuyItem}>
               <span className="buy_label">Comprar ahora</span>
             </button>
             &nbsp;*/}
-              <button className="btn btn-danger btn-buy" id={"btn-buy-" + props.product.id} onClick={handleAddItem}>
-                <span className="buy_label">agregar al <span className="icocart"> <HiShoppingCart /></span></span>
+              <button
+                className="btn btn-danger btn-buy"
+                id={"btn-buy-" + props.product.id}
+                onClick={handleAddItem}
+              >
+                <span className="buy_label">
+                  agregar al{" "}
+                  <span className="icocart">
+                    {" "}
+                    <HiShoppingCart />
+                  </span>
+                </span>
               </button>
             </div>
           </div>
         </div>
-        <div className="added_row">{addedMessage && <div className="agregado"><p>¡Producto agregado!</p></div>}</div>
+        <div className="added_row">
+          {addedMessage && (
+            <div className="agregado">
+              <p>¡Producto agregado!</p>
+            </div>
+          )}
+        </div>
       </div>
     </div>
-
   );
 };
 
